7d2c736c366321fe1fea3c50f62a7e3a31a9e68a
Phillip Smelt <phils@borisch.com>
2025-04-15 11:52:28 -0400

    4/15/25

--- TestInstaller/TestExecutiveInstaller.cs
+++ TestInstaller/TestExecutiveInstaller.cs
@@ -5,24 +5,41 @@ using System.Configuration.Install;
 using System.IO;
 using System.Security.AccessControl;
 using System.Security.Principal;
+using ABT.Test.TestExecutive.TestLib;
+using Microsoft.SqlServer.Server;
 
 namespace TestInstaller {
     [RunInstaller(true)]
     public partial class TestExecutiveInstaller : Installer {
-        private const String _ABT_TEST = @"\ABT\TEST";
-        private const String _BASEPATH_DATA = @"C:\ProgramData" + _ABT_TEST;
-        private const String _TEST_EXECUTIVE = @"\TestExecutive";
-        public const String TEST_EXECUTIVE_DATA = _BASEPATH_DATA + _TEST_EXECUTIVE;
-
-        public TestExecutiveInstaller() {
-            InitializeComponent();
-        }
+        // The below C:\Windows\System32\MSIexec.exe error:
+        //   === Logging started: 4/15/2025  10:47:52 ===
+        //   Error 1001. Error 1001. Exception occurred while initializing the installation:
+        //   System.BadImageFormatException: Could not load file or assembly 'file:///C:\Program Files\ABT\Test\TestExecutive\TestInstaller.dll' or one of its dependencies.
+        //   An attempt was made to load a program with an incorrect format..
+        //
+        // This C:\Windows\System32\MSIexec.exe error occurs:
+        // - If TestExecutiveInstaller is compiled as a 64-bit assembly, with the rest of
+        //   TestExecutive compiled as 64-bit and the target machine also 64-bit.
+        // - Compiling TestExecutiveInstaller as a 32-bit assembly, with the rest of
+        //   TestExecutive compiled as 64-bit and the target machine also 64-bit resolves this error.
+        // - C:\Windows\System32\MSIexec.exe is the 64-bit version of the Windows Installer:
+        //   - Speculate C:\Windows\System32\MSIexec.exe potentially invokes a 32 bit shim DLL somewhere, causing this error.
+        //
+        // Unfortunately, compiling TestExecutiveInstaller as a 32-bit assembly causes the following MSBuild Warning:
+        //   2>C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\amd64\Microsoft.Common.CurrentVersion.targets(2424,5):
+        //   warning MSB3270: There was a mismatch between the processor architecture of the project being built "x86"
+        //   and the processor architecture of the reference "C:\Users\phils\source\repos\ABT\Test\TestExecutive\TestLib\bin\x64\TestExecutive\TestLib.dll", "AMD64".
+        //   This mismatch may cause runtime failures.
+        //   Please consider changing the targeted processor architecture of your project through the Configuration Manager so as to align the processor architectures between your project and references,
+        //   or take a dependency on references with a processor architecture that matches the targeted processor architecture of your project.
+        // - But following this advice restores the original C:\Windows\System32\MSIexec.exe Error 1001.
+        // - Ignoring this warning still allows the installer to run successfully.
+        public TestExecutiveInstaller() { InitializeComponent(); }
 
         [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
         public override void Install(IDictionary stateSaver) {
             base.Install(stateSaver);
-            if (!Directory.Exists(TEST_EXECUTIVE_DATA)) Directory.CreateDirectory(TEST_EXECUTIVE_DATA);
-            DirectoryInfo directoryInfo = new DirectoryInfo(TEST_EXECUTIVE_DATA);
+            DirectoryInfo directoryInfo = new DirectoryInfo(Data.TEST_EXECUTIVE_DATA);
             DirectorySecurity directorySecurity = directoryInfo.GetAccessControl();
             directorySecurity.AddAccessRule(
                 new FileSystemAccessRule(
@@ -44,26 +61,9 @@ namespace TestInstaller {
         public override void Commit(IDictionary savedState) { base.Commit(savedState); }
 
         [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
-        public override void Rollback(IDictionary savedState) {
-            base.Rollback(savedState);
-            if (Directory.Exists(TEST_EXECUTIVE_DATA)) Directory.Delete(TEST_EXECUTIVE_DATA, true);
-        }
+        public override void Rollback(IDictionary savedState) { base.Rollback(savedState); }
 
         [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
-        public override void Uninstall(IDictionary savedState) {
-            //Process application = null;
-            //foreach (Process process in Process.GetProcesses()) {
-            //    if (!process.ProcessName.ToLower().Contains("yourprocessname")) continue;
-            //    application = process;
-            //    break;
-            //}
-
-            //if (application != null && application.Responding) {
-            //    application.Kill();
-            //    base.Uninstall(savedState);
-            //}
-            base.Uninstall(savedState);
-            if (Directory.Exists(TEST_EXECUTIVE_DATA)) Directory.Delete(TEST_EXECUTIVE_DATA, true);
-        }
+        public override void Uninstall(IDictionary savedState) { base.Uninstall(savedState); }
     }
 }
