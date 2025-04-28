31ab3251005206e4ba8bc890d55fd2704d203d3a
Phillip Smelt <phils@borisch.com>
2025-04-28 16:07:19 -0400

    4/28/25

--- TestLib/Configuration/TestSelect.cs
+++ TestLib/Configuration/TestSelect.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace ABT.Test.TestExecutive.TestLib.Configuration {
-    public partial class TestSelect : System.Windows.Forms.Form {
+    public partial class TestSelect : Form {
         private static TestSequence testSequence = new TestSequence();
 
         public TestSelect() {
@@ -22,15 +23,15 @@ namespace ABT.Test.TestExecutive.TestLib.Configuration {
 
         private void ListLoad() {
             TestList.Clear();
-            TestList.View = System.Windows.Forms.View.Details;
-            TestList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            TestList.View = View.Details;
+            TestList.HeaderStyle = ColumnHeaderStyle.Nonclickable;
             if (TestOperations.Checked) {
                 TestGroups.Checked = false;
                 Text = $"Select {nameof(TestOperation)}";
                 TestList.Columns.Add(nameof(TestOperation));
                 TestList.Columns.Add(nameof(TestGroup));
                 foreach (TestOperation testOperation in Data.testPlanDefinition.TestSpace.TestOperations)
-                    if (testOperation.ProductionTest) TestList.Items.Add(new System.Windows.Forms.ListViewItem(new String[] { testOperation.NamespaceTrunk, testOperation.Description }));
+                    if (testOperation.ProductionTest) TestList.Items.Add(new ListViewItem(new String[] { testOperation.NamespaceTrunk, testOperation.Description }));
             } else {
                 TestOperations.Checked = false;
                 Text = $"Select {nameof(TestGroup)}";
@@ -39,19 +40,19 @@ namespace ABT.Test.TestExecutive.TestLib.Configuration {
                 TestList.Columns.Add(nameof(TestGroup.Description));
                 foreach (TestOperation testOperation in Data.testPlanDefinition.TestSpace.TestOperations) {
                     foreach (TestGroup testGroup in testOperation.TestGroups)
-                        if (testGroup.Independent) TestList.Items.Add(new System.Windows.Forms.ListViewItem(new String[] { testOperation.NamespaceTrunk, testGroup.Classname, testGroup.Description }));
+                        if (testGroup.Independent) TestList.Items.Add(new ListViewItem(new String[] { testOperation.NamespaceTrunk, testGroup.Classname, testGroup.Description }));
                 }
             }
-            foreach (System.Windows.Forms.ColumnHeader ch in TestList.Columns) ch.Width = -2;
-            TestList.AutoResizeColumn(0, System.Windows.Forms.ColumnHeaderAutoResizeStyle.ColumnContent);
+            foreach (ColumnHeader ch in TestList.Columns) ch.Width = -2;
+            TestList.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.ColumnContent);
             if (TestList.Columns[0].Width < 115) TestList.Columns[0].Width = 115;
             TestList.ResetText();
             OK.Enabled = false;
         }
 
-        private void TestList_Changed(Object sender, System.Windows.Forms.ListViewItemSelectionChangedEventArgs e) { OK.Enabled = (TestList.SelectedItems.Count == 1); }
+        private void TestList_Changed(Object sender, ListViewItemSelectionChangedEventArgs e) { OK.Enabled = (TestList.SelectedItems.Count == 1); }
 
-        private void TestList_MouseDoubleClick(Object sender, System.Windows.Forms.MouseEventArgs e) { OK_Click(sender, e); }
+        private void TestList_MouseDoubleClick(Object sender, MouseEventArgs e) { OK_Click(sender, e); }
 
         private void OK_Click(Object sender, EventArgs e) {
             Debug.Assert(TestList.SelectedItems.Count == 1);
@@ -67,7 +68,7 @@ namespace ABT.Test.TestExecutive.TestLib.Configuration {
                 // From the selected TestOperation, retain only the selected TestGroup and all its Methods.
             }
 
-            DialogResult = System.Windows.Forms.DialogResult.OK;
+            DialogResult = DialogResult.OK;
         }
 
         private void TestOperations_Clicked(Object sender, EventArgs e) { ListLoad(); }
