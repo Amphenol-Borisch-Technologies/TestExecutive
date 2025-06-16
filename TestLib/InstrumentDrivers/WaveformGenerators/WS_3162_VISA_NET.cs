9b9a95b231173db0e2ba97b7a22d210b1ae52ac5
Phillip Smelt <phils@borisch.com>
2025-06-16 16:43:00 -0400

    6/16/25

--- TestLib/InstrumentDrivers/WaveformGenerators/WS_3162_VISA_NET.cs
+++ TestLib/InstrumentDrivers/WaveformGenerators/WS_3162_VISA_NET.cs
@@ -3,24 +3,109 @@ using Ivi.Visa;
 using Keysight.Visa;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Channels;
 
 namespace ABT.Test.TestExecutive.TestLib.InstrumentDrivers.WaveformGenerators {
     public class WS_3162_VISA_NET : IInstrument, IDiagnostics, IDisposable {
+        public enum CHANNELS { C1, C2 }
+        public enum CLOCK_SOURCE { INT, EXT }
+        public enum  COMMAND_HEADERS { OFF, SHORT, LONG }
+        public enum CONFIGURATIONS { DEFAULT, LAST }
+        public enum MINUTES { OFF, M1=1, M5=5, M15=15, M30=30, M60=60, M120=120, M300=300 }
+        public enum STATUSES { OFF, ON }
+
         public UsbSession UsbSession;
         public IMessageBasedFormattedIO FormattedIO => UsbSession.FormattedIO;
         public String Address { get; }
         public String Detail { get; }
         public INSTRUMENT_TYPES InstrumentType { get; }
 
+        public void BuzzerCommand(STATUSES Status) { FormattedIO.WriteLine($"*BUZZer {Status}"); }
+        public STATUSES BuzzerQuery() {
+            FormattedIO.WriteLine("*BUZZer?");
+            String response = FormattedIO.ReadLine();
+            return (STATUSES)Enum.Parse(typeof(STATUSES), response.Substring(response.IndexOf(" ") + 1), true);
+        }
+        public void ChannelParameterCopyCommand(CHANNELS ChannelSource) {
+            FormattedIO.WriteLine($"*PAraCoPy {(ChannelSource == CHANNELS.C1 ? CHANNELS.C2 : CHANNELS.C1)},{ChannelSource}");
+        }
+        public void ClearStatusCommand() { FormattedIO.WriteLine("*CLS"); }
+        public void ClockSourceCommand(CLOCK_SOURCE ClockSource) { FormattedIO.WriteLine($"*ROSCillator {ClockSource}"); }
+        public CLOCK_SOURCE ClockSourceQuery() {
+            FormattedIO.WriteLine("*ROSCillator?");
+            String response = FormattedIO.ReadLine();
+            return (CLOCK_SOURCE)Enum.Parse(typeof(CLOCK_SOURCE), response.Substring(response.IndexOf(" ") + 1), true);
+        }
+        public void CommandHeaderCommand(COMMAND_HEADERS CommandHeader) { FormattedIO.WriteLine($"*Comm_HeaDeR {CommandHeader}"); }
+        public COMMAND_HEADERS CommandHeaderQuery() {
+            FormattedIO.WriteLine("*Comm_HeaDeR?");
+            String response = FormattedIO.ReadLine();
+            return (COMMAND_HEADERS)Enum.Parse(typeof(COMMAND_HEADERS), response.Substring(response.IndexOf(" ") + 1), true);
+        }
+        public void ConfigurationCommand(CONFIGURATIONS Configuration) { FormattedIO.WriteLine($"*Sys_CFG {Configuration}"); }
+        public CONFIGURATIONS ConfigurationQuery() {
+            FormattedIO.WriteLine("*Sys_CFG?");
+            String response = FormattedIO.ReadLine();
+            return (CONFIGURATIONS)Enum.Parse(typeof(CONFIGURATIONS), response.Substring(response.IndexOf(" ") + 1), true);
+        }
+        public void EventStatusEnableCommand(Byte RegisterMask) { FormattedIO.WriteLine($"*ESE {RegisterMask}"); }
+        public Byte EventStatusEnableQuery() {
+            FormattedIO.WriteLine("*ESE?");
+            return Byte.Parse(FormattedIO.ReadLine().Substring(5));
+        }
+        public Byte EventStatusRegisterQuery() {
+            FormattedIO.WriteLine("*ESR?");
+            return Byte.Parse(FormattedIO.ReadLine().Substring(5));
+        }
+        public String IdentityQuery() {
+            FormattedIO.WriteLine("*IDN?");
+            return FormattedIO.ReadLine();
+        }
+        public void InvertCommand(STATUSES Status) { FormattedIO.WriteLine($"*INVerT {Status}"); }
+        public STATUSES InvertQuery() {
+            FormattedIO.WriteLine("*INVerT?");
+            String response = FormattedIO.ReadLine();
+            return (STATUSES)Enum.Parse(typeof(STATUSES), response.Substring(response.IndexOf(" ") + 1), true);
+        }
+        public void OperationCompleteCommand() { FormattedIO.WriteLine($"*OPC"); }
+        public Byte OperationCompleteQuery() {
+            FormattedIO.WriteLine($"*OPC?");
+            return Byte.Parse(FormattedIO.ReadLine().Substring(5));
+        }
+        public void ScreenSaveCommand(MINUTES Minutes) {
+            if (Minutes == MINUTES.OFF) FormattedIO.WriteLine($"SCreen_SaVe {Minutes}");
+            else FormattedIO.WriteLine($"SCreen_SaVe {(Int32)Minutes}");
+        }
+        public void ServiceRequestEnableCommand(Byte RegisterMask) { FormattedIO.WriteLine($"*SRE {RegisterMask}"); }
+        public Byte ServiceRequestEnableQuery() {
+            FormattedIO.WriteLine("*SRE?");
+            return Byte.Parse(FormattedIO.ReadLine().Substring(5));
+        }
+        public Byte StatusRegisterQuery() {
+            FormattedIO.WriteLine("*STB?");
+            return Byte.Parse(FormattedIO.ReadLine().Substring(5));
+        }
+        public void SynchronizeCommand(CHANNELS Channel, STATUSES Status) { FormattedIO.WriteLine($"{Channel}:SYNC {Status}"); }
+        public CHANNELS SynchronizeQuery() {
+            FormattedIO.WriteLine("*SYNC?");
+            String response = FormattedIO.ReadLine();
+            return (CHANNELS)Enum.Parse(typeof(CHANNELS), response.Substring(response.IndexOf(" ") + 1), true);
+        }
+        public String TestQuery() {
+            FormattedIO.WriteLine("*TST?");
+            return FormattedIO.ReadLine().Substring(5);
+        }
+        public void ResetCommand() { FormattedIO.WriteLine("*RST"); }
+        public void WaitCommand() { FormattedIO.WriteLine("*WAI"); }
         public void ResetClear() {
-            FormattedIO.WriteLine("*RST");
-            FormattedIO.WriteLine("*CLS");
+            ResetCommand();
+            ClearStatusCommand();
         }
 
         public SELF_TEST_RESULTS SelfTests() {
             try {
                 FormattedIO.WriteLine("*TST?");
-                if (Int32.TryParse(FormattedIO.ReadLine(), out Int32 i) && i == 0) return SELF_TEST_RESULTS.PASS;
+                if (TestQuery().Equals("0")) return SELF_TEST_RESULTS.PASS;
                 return SELF_TEST_RESULTS.FAIL;
             } catch (Exception exception) {
                 Instruments.SelfTestFailure(this, exception);
