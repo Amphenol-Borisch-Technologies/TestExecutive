5c1c1bef4e41c0144b0e9e0ba07479479c6a5438
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2026-02-19 16:19:38 -0500

    2/19/26

---
+++ TestLib/Miscellaneous/UUT_Connections.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ABT.Test.TestExecutive.TestLib.Miscellaneous {
+    public partial class UUT_Connections : Form {
+        public UUT_Connections(Image image) {
+            InitializeComponent();
+            BackgroundImage = image;
+        }
+
+        private void Completed_Click(System.Object sender, System.EventArgs e) {
+            Close();
+        }
+    }
+}
